4fc8b4a88325d93f3a38daf092836c691a1452b6
Rich Lysogorski <rmlysogorski@gmail.com>
2019-04-24 16:59:48 -0400

    Grand Circus C# .NET April 2019 Lab Number 6

---
+++ Lab6Take2/DiceRollerApp.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6Take2
+{
+    class DiceRollerApp
+    {
+        private static readonly Random rnd = new Random();
+
+        public static void RollTheDice(int count)
+        {
+            int die1 = 0, die2 = 0, sides = 0;
+
+            sides = GetNumber("\nHow many sides should each die have? (2-20): ");
+            die1 = GetRandomNumber(sides);
+            die2 = GetRandomNumber(sides);
+            PrintResults(die1, die2, count);
+            CheckForSpecialRoll(die1, die2);
+            
+        }
+
+        public static int GetNumber(string message)
+        {
+            Console.Write(message);
+            if (int.TryParse(Console.ReadLine(), out int number) && number <= 20 && number >= 2)
+            {
+                return number;
+            }
+            else
+            {
+                Console.WriteLine("Not a valid input!");
+                return GetNumber(message);
+            }
+        }
+
+        public static int GetRandomNumber(int sides)
+        {            
+            return rnd.Next(1, sides + 1);
+        }
+
+        public static void PrintResults(int die1, int die2, int count)
+        {
+            Console.WriteLine($"Roll {count}:\n{die1}\n{die2}\n");
+        }
+
+        public static void CheckForSpecialRoll(int die1, int die2)
+        {
+            if (die1 == 1 && die2 == 1)
+            {
+                Console.WriteLine("Snake Eyes!\n");
+            }
+            else if (die1 + die2 == 7)
+            {
+                Console.WriteLine("Craps!\n");
+            }
+            else if (die1 == 6 && die2 == 6)
+            {
+                Console.WriteLine("Box Cars!\n");
+            }
+        }
+
+        public static bool GetUserChoice(string message, string option1, string option2)
+        {
+            Console.Write(message);
+            string choice = Console.ReadLine();
+            bool answer;
+
+            if(choice == option1)
+            {
+                answer = true;
+            }
+            else if (choice == option2)
+            {
+                answer = false;
+            }
+            else
+            {
+                Console.WriteLine("Not a valid option!");
+                return GetUserChoice(message, option1, option2); 
+            }
+
+            return answer;
+        }
+    }
+}
---
+++ Lab6Take2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6Take2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool choice;
+            int count = 0;
+
+            Console.Clear();
+            Console.WriteLine("Welcome to  the Grand Circus Casino!");
+            Console.WriteLine("------------------------------------");
+            choice = DiceRollerApp.GetUserChoice("Would you like to roll the dice? (y/n)", "y", "n");
+
+            if (choice)
+            {
+                count++;
+            }
+
+            while (choice)
+            {
+                DiceRollerApp.RollTheDice(count);
+                count++;
+                choice = DiceRollerApp.GetUserChoice("Roll Again? (y/n)", "y", "n");
+            }
+
+            if(count == 1)
+            {
+                Console.WriteLine($"\nYou rolled the dice {count} time.\n");
+            }
+            else
+            {
+                Console.WriteLine($"\nYou rolled the dice {count} times.\n");
+            }
+            
+        }
+    }
+}
